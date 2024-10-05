34358ef38d747126846b738d9691d836dacdec70
ni <revecoFuentesd@gmail.com>
2024-10-04 23:41:14 -0300

    Mejoras en la Vista rooms, agregando un localStorage de cliente, previo a la api de reserva

--- Proyecto/HotelApp/src/app/rooms/rooms.page.ts
+++ Proyecto/HotelApp/src/app/rooms/rooms.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AlertController } from '@ionic/angular';
+import { AlertController,ModalController } from '@ionic/angular';
 import { ApiService } from '../api.service';
+import { OverlayEventDetail } from '@ionic/core/components';
+
 
 
 @Component({
@@ -10,16 +12,32 @@ import { ApiService } from '../api.service';
   styleUrls: ['./rooms.page.scss'],
 })
 export class RoomsPage implements OnInit {
+  message = 'This modal example ';
   opcion: string = '';
   fecha: string = '';
   count: number = 0;
+  precio: string = '';
+  habitacionSeleccionada: string = '';
+  id: number = 0;
+  correo: any = '';
+  contrasenia: any = '';
+  idCliente: any = '';
+  
 
   constructor(
     private router: Router,
     private alertController: AlertController,
-    private apiService: ApiService 
-  ) { }
+    private apiService: ApiService ,
+    private modalController: ModalController,
 
+  ) {
+    this.correo = localStorage.getItem('correo');
+    this.contrasenia = localStorage.getItem('contrasenia');
+    this.idCliente =Number(localStorage.getItem('idCliente')) ;
+  
+   }
+
+ 
   ngOnInit() {
   }
 
@@ -36,6 +54,42 @@ export class RoomsPage implements OnInit {
     this.count--;
   }
 
+  seleccionarHabitacion(habitacion: string, id: number, precio: string) {
+    this.habitacionSeleccionada = habitacion;
+    this.id = id;
+    this.precio = precio
+  }
+  onWillDismissDepositarme(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<number>>;
+    if (ev.detail.role === 'confirm') {
+     this.message = `Hello, ${ev.detail.data}!`;
+    }
+  }
+
+  onWillDismissDepositar(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<number>>;
+    if (ev.detail.role === 'confirm') {
+     this.message = `Hello, ${ev.detail.data}!`;
+    }
+  }
+
+  cancel() {
+    this.modalController.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modalController.dismiss(null, 'confirm');
+  }
+
+  async mostrarAlerta(titulo: string, mensaje: string) {
+    const alert = await this.alertController.create({
+      header: titulo,
+      message: mensaje,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
   
 
 }
